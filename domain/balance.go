8ce9e1c8018ccc18e8a57a45db96ec77fe9fe267
Daniil Dulin <daniildulin@gmail.com>
2021-03-15 16:38:46 +0300

    fix

--- domain/balance.go
+++ domain/balance.go
@@ -1,7 +1,6 @@
 package domain
 
 type Balance struct {
-	ID        uint64 `json:"id"`
 	AddressID uint64 `json:"address_id" pg:",pk"`
 	CoinID    uint64 `json:"coin_id"    pg:",pk,use_zero"`
 	Value     string `json:"value"      pg:"type:numeric(70)"`
