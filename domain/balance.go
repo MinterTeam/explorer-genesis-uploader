7348ea1e56ed81c7a0115aa60b47f2ba23f382a7
Daniil Dulin <daniildulin@gmail.com>
2021-03-15 16:31:43 +0300

    update balance model

--- domain/balance.go
+++ domain/balance.go
@@ -1,8 +1,8 @@
 package domain
 
 type Balance struct {
-	ID        uint64 `json:"id" pg:",pk"`
-	AddressID uint64 `json:"address_id"`
-	CoinID    uint64 `json:"coin_id"  pg:",use_zero"`
-	Value     string `json:"value"    pg:"type:numeric(70)"`
+	ID        uint64 `json:"id"`
+	AddressID uint64 `json:"address_id" pg:",pk"`
+	CoinID    uint64 `json:"coin_id"    pg:",pk,use_zero"`
+	Value     string `json:"value"      pg:"type:numeric(70)"`
 }
